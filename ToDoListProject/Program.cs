b038d9acd9b98ffcbece677a3171a29fd36ca836
Robert Taylor <roycetaylor.rt@gmail.com>
2022-09-08 21:26:12 -0400

    Implimented Completed List Feature

--- ToDoListProject/Program.cs
+++ ToDoListProject/Program.cs
@@ -8,32 +8,42 @@ namespace ToDoListProject
     internal class Program
     {
         //To Do:
-        //Use Regex to grab the line number
-        //Figure out marking as complete
         //Figure out deleting
 
         static void Main(string[] args)
         {
-            //The path the file is in (change for release)
+            #region
+            //The paths the files are in
             string path = @"list.txt";
-            //Creates the list that stores the tasks
+            string pathComplete = @"list-complete.txt";
+
+            //Lists that store the tasks
             List<string> toDoList = new List<string>();
-            //Creates the list to store the completed tasks
             List<string> toDoListComplete = new List<string>();
 
-            //Stores previous lines within todo list upon opening
+            //Stores previous lines within to-do list upon opening
             string[] readFile = File.ReadAllLines(path);
+            string[] readFileComplete = File.ReadAllLines(pathComplete);
             
             //AddRange in to replace a foreach loop (Khalil taught me)
             toDoList.AddRange(readFile);
-            
-            ForEachView(toDoList);
+            toDoListComplete.AddRange(readFileComplete);
+            #endregion
+
 
             //While loop to write to the file
             while (true)
             {
+                //Displays the Graphics
+                Console.Clear();
+                Console.WriteLine("----- ToDo List -----");
+                ForEachView(toDoList);
+                Console.WriteLine("----- Completed -----");
+                ForEachViewComplete(toDoListComplete);
+
                 //Takes the user input method and stores it in a string
                 string input = UserInput();
+                
                 //If the conditions are met, will exit out of the program
                 if (input == "#Save" || input == "#S")
                 {
@@ -41,38 +51,41 @@ namespace ToDoListProject
                 }
                 else if (input == "#Check" || input == "#C")
                 {
-                    Console.Clear();
-                    //Figure out:
-                        //Why if you make the same entry the index doesn't change
-                        //How to mark an item as complete
+                    MarkTaskComplete(toDoList,toDoListComplete); 
                 }
                 else
                 {
                     //Adds the user input to the list
                     toDoList.Add(input);
-                    Console.Clear();
-                    //Displays the list then starts the loop over just displaying the list
-                    ForEachView(toDoList);
                 }
             }
+            //Exiting the Program
             Console.Clear();
+            Console.WriteLine("----- ToDo List -----");
             ForEachView(toDoList);
-
+            Console.WriteLine("----- Completed -----");
+            ForEachViewComplete(toDoListComplete);
+            Console.WriteLine("======================");
+            Console.WriteLine("Files saved. \nPress any key to quit...");
+            Console.Read();
+            //Saves the lists to the files
             File.WriteAllLines(path, toDoList);
+            File.WriteAllLines(pathComplete, toDoListComplete);
         }
 
         //Takes the user input for the tasks on the list
         private static string UserInput()
         {
-            Console.WriteLine("=====================");
-            Console.WriteLine("-----Add to List-----");
-            Console.WriteLine("=====================");
-            Console.WriteLine("  Type #Save to Save");
-            //Console.WriteLine("Type #Check to Check Off Item");
-            Console.WriteLine("=====================");
-            //More Instructions needed
-            string input = Console.ReadLine();
-            return input;
+            //Displays instructions
+            Console.WriteLine("=======================");
+            Console.WriteLine("----- Add to List -----");
+            Console.WriteLine("=======================");
+            Console.WriteLine("- Type #Save to Save -");
+            Console.WriteLine("Type #Check to Check Off Item");
+            Console.WriteLine("=======================");
+            Console.Write("> ");
+            string userInput = Console.ReadLine();
+            return userInput;
 
         }
 
@@ -85,10 +98,44 @@ namespace ToDoListProject
                 Console.WriteLine($"{lines.IndexOf(line) + 1}[] - {line}");
             }
         }
-
-        private static void MarkComplete(List<string> lines,int userInput)
+        private static void ForEachViewComplete(List<string> lines)
         {
-            lines.RemoveAt(userInput-1);
+            foreach (string line in lines)
+            {
+                //prints the index a check box and the line
+                Console.WriteLine($"{lines.IndexOf(line) + 1}[X] - {line}");
+            }
+        }
+
+        //Marks the files as complete(Takes in both lists)
+        private static void MarkTaskComplete(List<string> toDoList, List<string> toDoListComplete)
+        {
+            while (true)
+            {
+                //Displays Graphics
+                Console.Clear();
+                ForEachView(toDoList);
+                Console.WriteLine("======================");
+                Console.WriteLine("-- Select line item --");
+                Console.WriteLine("======================");
+                Console.WriteLine("- Type #Exit to Exit -");
+                Console.WriteLine("======================");
+                Console.Write("> ");
+
+                string userInputCheck = Console.ReadLine();
+                if(userInputCheck == "#Exit" || userInputCheck == "#E")
+                {
+                    break;
+                }
+                bool userInputCheckParse = Int32.TryParse(userInputCheck, out int userInputCheckInt);
+                int lengthOfToDoList = toDoList.Count;
+                if (userInputCheckParse && userInputCheckInt <= lengthOfToDoList)
+                {
+                    toDoListComplete.Add(toDoList[userInputCheckInt-1]);
+                    toDoList.RemoveAt(userInputCheckInt - 1);
+
+                }
+            }
         }
 
     }
